ce9526c778b175102eb7d6c7d91b3c0124c9db09
Yinzhe_Qi <goblin-qyz@163.com>
2020-08-13 00:03:25 +0800

    feat: upgrade deps, switch to styled-components

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,25 +1,58 @@
 import React from 'react';
-import './App.scss';
-const logo = require('./logo.svg');
+import logo from './logo.svg';
+import styled from 'styled-components';
+
+const App_ = styled.div`
+  text-align: center;
+`;
+
+const AppLogo_ = styled.img`
+  animation: App-logo-spin infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+  @keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+`;
+
+const AppHeader_ = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppLink_ = styled.a`
+  color: #61dafb;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <App_>
+      <AppHeader_>
+        <AppLogo_ src={logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <AppLink_
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </AppLink_>
+      </AppHeader_>
+    </App_>
   );
 }
 
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createGlobalStyle } from 'styled-components';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+`;
+
+ReactDOM.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  document.getElementById('app')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
